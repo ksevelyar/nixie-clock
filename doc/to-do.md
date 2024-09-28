6c2ed2a6f52ab5f7a12e076eb0d1a3cf2a551f2e
Sergey Zubkov <ksevelyar@protonmail.com>
2024-09-28 13:55:14 +0300

    add flakes

---
+++ doc/to-do.md
@@ -0,0 +1,2 @@
+* 22awg 0.64 mm wire for breadboard
+* build generator with esp32-c3
---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+  };
+
+  outputs = { nixpkgs, flake-utils, rust-overlay, ... }:
+    flake-utils.lib.eachSystem [ "x86_64-linux" ] (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ rust-overlay.overlays.default ];
+        };
+      in
+      {
+        devShell = pkgs.mkShell
+          {
+            nativeBuildInputs = with pkgs; [
+              (
+                rust-bin.stable.latest.default.override {
+                  targets = [
+                    "riscv32imc-unknown-none-elf"
+                  ];
+                }
+              )
+              cargo-generate
+              ldproxy
+              espup
+              espflash
+              cargo-espflash
+
+              minicom
+            ];
+          };
+      }
+    );
+}
